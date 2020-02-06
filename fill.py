b006b58f3094965510d065203623c06e3c8d2686
Lyell Read <lyellread@gmail.com>
2020-02-05 21:14:46 -0800

    Minor changes

--- fill.py
+++ fill.py
@@ -9,8 +9,8 @@ n = random.randint(8, 16)
 for x in range (0,n):
         running += gen.paragraph()
 
-#os.system('''echo "Starting Write Process, select target in next 20 seconds" | wall''')
 print("Chosen n: ", n)
+print("You have 20 seconds before data entry starts; select the window to fill.")
 time.sleep(20)
 
 os.system('''xdotool type "''' + running.replace('''"''',"").replace("(", "").replace(")", "") + '''"''')
